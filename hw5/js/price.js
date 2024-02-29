bffd95e7d30c89b9a7c95b702f3850eefab01514
AC16777216 <146387944+AC16777216@users.noreply.github.com>
2024-02-28 20:41:46 -0500

    hw5 so far

--- hw5/js/price.js
+++ hw5/js/price.js
@@ -83,6 +83,8 @@ class Roll {
         this.glazing =  rollGlazing;
         this.size = packSize;
         this.basePrice = basePrice;
+
+		this.element = null;
     }
 }
 
@@ -91,3 +93,84 @@ function printCart() {
 	cart.push(roll);
 	console.log(cart);
 }
+
+// Adds a roll to the cart, and creates an element for it 
+function addToCart(newRoll) { 
+	cart.push(newRoll);
+	createRollElement(newRoll);
+}
+
+function getPrice(roll) {
+	const glazingPrice = glazingPrices[roll.rollGlazing];
+	const packPrice = packPrices[roll.packSize];
+	return (roll.basePrice + glazingPrice) * packPrice;
+}
+
+function updateTotalPrice() {
+	const totalPriceElement = document.querySelector('.total-price');
+	let totalPriceTemp = 0;
+	for (const roll in cart) {
+		totalPriceTemp += getPrice(roll);
+	}
+	totalPriceElement.innerHTML = "$ " + totalPriceTemp;
+}
+
+// Repurposed from createElement(notecard) from lab06
+function createRollElement(newRoll) {
+	// make a clone of the cart-item template
+	const template = document.querySelector('.cart-item-template');
+	const clone = template.content.cloneNode(true);
+	newRoll.element = clone.querySelector('.cart-item');
+
+	// update image, details, price of clone
+	const newRollImage = newRoll.element.querySelector('.product-image');
+	newRollImage.src = rolls[newRoll.rollType]["imageFile"];
+
+	const newRollName = newRoll.element.querySelector('#detail-name');
+	newRollName.innerText = newRoll.rollType + " Cinnamon Roll";
+	const newRollGlaze = newRoll.element.querySelector('#detail-glaze');
+	newRollGlaze.innerText = newRoll.rollGlazing;
+	const newRollSize = newRoll.element.querySelector('#detail-size');
+	newRollSize.innerText = newRoll.packSize;
+
+	const newRollPrice = newRoll.element.querySelector('item-price');
+	newRollPrice.innerText = getPrice(newRoll);
+  
+	// delete button implementation
+	const btnDelete = notecard.element.querySelector('.remove');
+	console.log(btnDelete);
+	btnDelete.addEventListener('click', () => {
+	  deleteRollElement(newRoll);
+	});
+	
+	// add the notecard clone to the DOM
+	// find the notecard parent (#notecard-list) and add our notecard as its child
+	const cartElement = document.querySelector('.cart-wrapper');
+	cartElement.prepend(newRoll.element);
+	
+	// UNUSED - populate the notecard clone with the actual notecard content
+	//updateElement(notecard);
+	updateTotalPrice();
+}
+
+  // // Repurposed from deleteNote(notecard) from lab06
+function deleteRollElement(delRoll) {
+	// remove the Roll DOM object from the UI
+	delRoll.element.remove();
+  
+	// remove the actual Roll object from cart
+	const index = cart.indexOf(delRoll);
+	if (index > -1) { // don't remove anything if nothing is found
+		cart.splice(index, 1); 
+	}
+
+	updateTotalPrice();
+}
+
+const roll1 = new Roll("Original", "Sugar milk", 1, rolls["Original"]["basePrice"]);
+addToCart(roll1);
+console.log("roll1");
+addToCart(new Roll("Walnut", "Vanilla milk", 12, rolls["Walnut"]["basePrice"]));
+addToCart(new Roll("Raisin", "Sugar milk", 3, rolls["Raisin"]["basePrice"]));
+addToCart(new Roll("Apple", "Original", 3, rolls["Apple"]["basePrice"]));
+console.log("Cart: " + cart);
